a88b6dbac428ed349ee3dee9a582bd4cef2a1b14
nicoleseg <nicole.segarra23@trinityschoolnyc.org>
2023-01-15 17:21:12 -0500

    claen up, two score boards, fighter create

--- server.js
+++ server.js
@@ -46,7 +46,6 @@ app.get('/play2', function(request, response) {
     let opponent = request.query.opponent;
     let word = request.query.word;
     let fighter = request.query.fighter;
-    let playerThrow = request.query.throw;
 
     if(opponents[opponent] || words[word] || fighters[fighter]){
       let wordlist = [];
@@ -77,13 +76,19 @@ app.get('/play2', function(request, response) {
       function win(){ //opponents meaning users
         opponents[opponent].win++;
         fighters[fighter].lose++;
+        fighters[fighter].time--;
       }
 
       function timer(){
         gameTime++;
-        if (gameTime == fighters[fighter].time){
+        if (gameTime < fighters[fighter].time){
+          //if (chosenWord==guessWord){
+          //win();
+          //}
+        }
+        else {
+          //
           lose();
-          //console.log("Lost! time = " + gameTime + ", fighterTime = " + fighters[fighter].time);
         }
       }
 
@@ -138,67 +143,9 @@ app.get('/play2', function(request, response) {
     setInterval(timer, 1000);
 });
 
-app.get('/results', function(request, response) {
+app.get('/opponentsScores', function(request, response) {
   let opponents = JSON.parse(fs.readFileSync('data/opponents.json'));
-  let fighters = JSON.parse(fs.readFileSync('data/fighters.json'));
-    //accessing URL query string information from the request object
-    let opponent = request.query.opponent;
-    let fighter = request.query.fighter;
-    let playerThrow = request.query.throw;
-
-    if(opponents[opponent]){
-      let opponentThrowChoices=["Paper", "Rock", "Scissors"];
-      let results={};
-
-      results["opponentGuess"]=opponentGuess;
-      results["opponentName"]=opponent;
-      results["opponentPhoto"]=opponents[opponent].photo;
-      //results["opponentTime"]=gameTime;
-      results["fighterName"]=fighter;
-      results["fighterPhoto"]=fighters[fighter].photo;
-      results["fighterTime"]=fighters[fighter].time;
-      results["chosenWord"] = chosenWord;
-
-      /*if(results["opponentTime"]<=results["fighterTime"]){
-      if(results["opponentGuess"]===results["chosenWord"]){
-        results["outcome"] = "win";
-      }else if(results["opponentGuess"]!=results["chosenWord"]){
-        results["outcome"] = "lose";
-      }
-      }else{
-        results["outcome"] = "lose";
-      }
-
-      if(results["outcome"]=="win"){
-        opponents[opponent]["win"]++;
-        fighters[fighter]["time"]++;
-      }
-      else opponents[opponent]["lose"]++; */
-
-      //update opponents.json to permanently remember results
-      fs.writeFileSync('data/opponents.json', JSON.stringify(opponents));
-      fs.writeFileSync('data/words.json', JSON.stringify(words));
-      fs.writeFileSync('data/fighters.json', JSON.stringify(fighters));
-
-      response.status(200);
-      response.setHeader('Content-Type', 'text/html')
-      response.render("results", {
-        data: results
-      });
-    }else{
-      response.status(404);
-      response.setHeader('Content-Type', 'text/html')
-      response.render("error", {
-        "errorCode":"404"
-      });
-    }
-});
-
-app.get('/scores', function(request, response) {
-  let opponents = JSON.parse(fs.readFileSync('data/opponents.json'));
-  let fighters = JSON.parse(fs.readFileSync('data/fighter.json'));
   let opponentArray=[];
-  let fighterArray=[];
 
   //create an array to use sort, and dynamically generate win percent
   for(name in opponents){
@@ -209,20 +156,30 @@ app.get('/scores', function(request, response) {
   opponentArray.sort(function(a, b){
     return parseFloat(b.win_percent)-parseFloat(a.win_percent);
   })
-  //create an array to use sort, and dynamically generate win percent
+
+  response.status(200);
+  response.setHeader('Content-Type', 'text/html')
+  response.render("opponentsScores",{
+    opponents: opponentArray,
+  });
+});
+
+app.get('/fightersScores', function(request, response) {
+  let fighters = JSON.parse(fs.readFileSync('data/fighters.json'));
+  let fighterArray=[];
+
   for(name in fighters){
-    fighters[name].currentTime = (fighters[name].time - 5);
-    if(fighters[name].currentTime=="NaN") fighters[name].currentTime=0;
+    fighters[name].win_percent = (fighters[name].win/parseFloat(fighters[name].win+fighters[name].lose) * 100).toFixed(2);
+    if(fighters[name].win_percent=="NaN") fighters[name].win_percent=0;
     fighterArray.push(fighters[name])
   }
   fighterArray.sort(function(a, b){
-    return parseFloat(b.currentTime)-parseFloat(a.currentTime);
+    return parseFloat(b.win_percent)-parseFloat(a.win_percent);
   })
 
   response.status(200);
   response.setHeader('Content-Type', 'text/html')
-  response.render("scores",{
-    opponents: opponentArray,
+  response.render("fightersScores",{
     fighters: fighterArray,
   });
 });
@@ -234,13 +191,38 @@ app.get('/opponent/:opponentName', function(request, response) {
   let opponentName = request.params.opponentName;
 
   if(opponents[opponentName]){
-    opponents[opponentName].win_percent = (opponents[opponentName].win/parseFloat(opponents[opponentName].win+opponents[opponentName].lose+opponents[opponentName].tie) * 100).toFixed(2);
+    opponents[opponentName].win_percent = (opponents[opponentName].win/parseFloat(opponents[opponentName].win+opponents[opponentName].lose) * 100).toFixed(2);
     if(opponents[opponentName].win_percent=="NaN") opponents[opponentName].win_percent=0;
 
     response.status(200);
     response.setHeader('Content-Type', 'text/html')
     response.render("opponentDetails",{
       opponent: opponents[opponentName]
+    });
+
+  }else{
+    response.status(404);
+    response.setHeader('Content-Type', 'text/html')
+    response.render("error", {
+      "errorCode":"404"
+    });
+  }
+});
+
+app.get('/fighter/:fighterName', function(request, response) {
+  let fighters = JSON.parse(fs.readFileSync('data/fighters.json'));
+
+  // using dynamic routes to specify resource request information
+  let fighterName = request.params.fighterName;
+
+  if(fighters[fighterName]){
+    fighters[fighterName].win_percent = (fighters[fighterName].win/parseFloat(fighters[fighterName].win+fighters[fighterName].lose) * 100).toFixed(2);
+    if(fighters[fighterName].win_percent=="NaN") fighters[fighterName].win_percent=0;
+
+    response.status(200);
+    response.setHeader('Content-Type', 'text/html')
+    response.render("fighterDetails",{
+      fighter: fighters[fighterName]
     });
 
   }else{
@@ -268,7 +250,6 @@ app.post('/opponentCreate', function(request, response) {
         "photo": opponentPhoto,
         "win":0,
         "lose": 0,
-        "tie": 0,
       }
       opponents[opponentName] = newOpponent;
       fs.writeFileSync('data/opponents.json', JSON.stringify(opponents));
@@ -294,17 +275,18 @@ app.get('/fighterCreate', function(request, response) {
 app.post('/fighterCreate', function(request, response) {
     let fighterName = request.body.fighterName;
     let fighterPhoto = request.body.fighterPhoto;
-    if(fighterName&&fighterPhoto){
-      let fighter = JSON.parse(fs.readFileSync('data/fighter.json'));
+    let fighterTime = request.body.fighterTime;
+    if(fighterName&&fighterPhoto&&fighterTime){
+      let fighters = JSON.parse(fs.readFileSync('data/fighters.json'));
       let newFighter={
         "name": fighterName,
         "photo": fighterPhoto,
+        "time": fighterTime,
         "win":0,
         "lose": 0
       }
-      fighter[fighterName] = newFighter;
+      fighters[fighterName] = newFighter;
       fs.writeFileSync('data/fighters.json', JSON.stringify(fighters));
-
       response.status(200);
       response.setHeader('Content-Type', 'text/html')
       response.redirect("/fighter/"+fighterName);
